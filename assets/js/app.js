cc6394e90e1452599e6ee4eb5a9112977c837179
IsisSilva <izizvsilva@gmail.com>
2024-08-26 16:26:07 -0600

    Cambios sesion 2 CH44

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name');//Se cambio la función al id correcto con el #
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+
+  async function name() { // Se pone una función asincrona ya que se esta realizando una promesa
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const usersEndpoint = await response.json();//Se pasa a Fetch en formato json para que puedan ser leidos
+  const username = await response.json();
   console.log(data);
+  displayUser(username); //Se pone abajo ya que no estaba definida
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
